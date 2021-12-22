9024e6473b346c6bf09c89980809b5b519e1d6f9
kimkyury <mandarining0918@gmail.com>
2021-12-22 10:26:39 +0900

    chore

--- Assignment/Source.cpp
+++ Assignment/Source.cpp
@@ -35,37 +35,67 @@ int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmd
 	return Message.wParam;
 }
 
-/* 시간 구조체 */
-struct ThreadParam {
-	int hour, minute, second;
-	TCHAR ch;
-} time;
+
 
 int i = 0;
 int totalSec;
 SYSTEMTIME	st;
+HWND btn_setting,btn_init;
+HWND edit_h, edit_m, edit_s;
+
+TCHAR time[50];
+int hour, minute, second;
+
+bool mode = false; // true면 타이머, false면 현재시간
+
+
+
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 
 	PAINTSTRUCT ps;
 	HDC hdc;
 	static TCHAR sTime[128];
+	static TCHAR timeUnit[10];
 	HPEN  MyPen, OldPen;
 	HBRUSH MyBrush, OldBrush;
-
 	switch (message){
 
 		// 타이머 리셋시키기
 		// 타이머  
-
 	case WM_CREATE:
+		btn_setting = CreateWindow("button", "설정", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 350, 350, 100, 25, hWnd, (HMENU)0x1001, g_hInst, NULL);
+		btn_init = CreateWindow("button", "초기화", WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON, 350, 380, 100, 25, hWnd, (HMENU)0x1002, g_hInst, NULL);
+
+
+		edit_h = CreateWindow("edit", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, 100, 350, 50, 25, hWnd, (HMENU)0x1003, g_hInst, NULL);
+		edit_m = CreateWindow("edit", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, 180, 350, 50, 25, hWnd, (HMENU)0x1004, g_hInst, NULL);
+		edit_s = CreateWindow("edit", NULL, WS_CHILD | WS_VISIBLE | WS_BORDER | ES_NUMBER, 260, 350, 50, 25, hWnd, (HMENU)0x1005, g_hInst, NULL);
+
+		
+		hour = 0, minute = 0, second = 0;
 		SetTimer(hWnd, 1, 1000, NULL);
+		
 		return 0;
 
 	case WM_TIMER:
-		totalSec++; //시침 올라감
 		hdc = BeginPaint(hWnd, &ps);
 		GetLocalTime(&st);
-		wsprintf(sTime, "남은 시간 :  %d시 %d분 %d초", st.wHour, st.wMinute, st.wSecond);
+
+		if (mode == true){ //타이머 모드
+			second--; //
+			if (second % 60 == 0){
+				minute--;
+			}
+			if (second % 3600 == 0){
+				hour--;
+			}
+			wsprintf(sTime, "남은 시간 :  %d시 %d분 %d초", hour, minute, second);
+		}
+		else{   //현재시각 모드
+			wsprintf(sTime, "현재 시각 :  %d시 %d분 %d초", st.wHour, st.wMinute, st.wSecond);
+		}
+
 		InvalidateRect(hWnd, NULL, true);
 		return 0;
 
@@ -74,16 +104,33 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 		// 버튼 = 세팅버튼, 에디터박스: 시, 분, 초
 		// 버튼 클릭시 초기화한거니 전역totalSec=0으로 초기화
 		switch (LOWORD(wParam)){
+		case 0x1001:
+			mode = true; // 타이머 모드 발동
+			GetWindowText(edit_h, time, 50);
+			hour = (int)time;
+			GetWindowText(edit_m, time, 50);
+			minute = (int)time;
+			GetWindowText(edit_s, time, 50);
+			second = (int)time;
 
+			hour = (int)time;
+			//InvalidateRect(hWnd, NULL, true);
+			break;
 
+		case 0x1002:
+			mode = false; // 현재시각, 초기화
+			hour = 0;
+			minute = 0;
+			second = 0;
+			//InvalidateRect(hWnd, NULL, true);
+			break;
 		}
 		return 0;
 
 	case WM_PAINT:
-
-		// 반대로 돌아가게 만들어야함
 		hdc = BeginPaint(hWnd, &ps);
 		i = st.wSecond;
+
 
 		MyBrush = (HBRUSH)CreateSolidBrush(RGB(255, 255, 0));
 		OldBrush = (HBRUSH)SelectObject(hdc, MyBrush);
@@ -95,35 +142,72 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam){
 		Ellipse(hdc, 198, 198, 202, 202); //시계 중심의 위치
 
 		SetTextAlign(hdc, TA_CENTER);
+
+		wsprintf(timeUnit, TEXT("시"));
+		TextOut(hdc, 160, 350, timeUnit, strlen(timeUnit));
+		wsprintf(timeUnit, TEXT("분"));
+		TextOut(hdc, 240, 350, timeUnit, strlen(timeUnit));
+		wsprintf(timeUnit, TEXT("초"));
+		TextOut(hdc, 320, 350, timeUnit, strlen(timeUnit));
+
 		TextOut(hdc, 200, 320, sTime, strlen(sTime)); // 남은 시간 출력위치, 시간 초기화는 초마다.
 
+		//시계 시각 표시
 		for (i = 1; i <= 12; i++){
 			wsprintf(sTime, "%d", i);
 			TextOut(hdc, int(202 + 89 * cos((270 + 30 * i) * 3.14 / 180)),
 				int(193 + 89 * sin((270 + 30 * i) * 3.14 / 180)), sTime, strlen(sTime));
 		}
 
-		MyPen = CreatePen(PS_DASH, 1, RGB(0, 0, 0));
-		OldPen = (HPEN)SelectObject(hdc, MyPen);
+		if (mode == true){
+			//반대로 돌아가게 만듦
+
+			//초침
+			MyPen = CreatePen(PS_DASH, 1, RGB(0, 0, 0));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 85 * cos((270 - 6 * second) * 3.14 / 180)),
+				int(200 + 85 * sin((270 - 6 * second) * 3.14 / 180)));
+
+			//분침
+			MyPen = CreatePen(PS_SOLID, 3, RGB(0, 255, 0));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 70 * cos((270 - 6 * minute + second / 10) * 3.14 / 180)),
+				int(200 + 70 * sin((270 - 6 * minute + second / 10) * 3.14 / 180)));
 
 
-		MoveToEx(hdc, 200, 200, NULL);
-		LineTo(hdc, int(200 + 85 * cos((270 + 6 * st.wSecond) * 3.14 / 180)),
-			int(200 + 85 * sin((270 + 6 * st.wSecond) * 3.14 / 180)));
+			//시침
+			MyPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 50 * cos((270 - 30 * hour + minute / 2) * 3.14 / 180)),
+				int(200 + 50 * sin((270 - 30 * hour + minute / 2) * 3.14 / 180)));
+		}
+		else {
+			// 아날로그 시계처럼 돌아가게 만들어야함
 
-		MyPen = CreatePen(PS_SOLID, 3, RGB(0, 255, 0));
-		OldPen = (HPEN)SelectObject(hdc, MyPen);
+			//초침
+			MyPen = CreatePen(PS_DASH, 1, RGB(0, 0, 0));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 85 * cos((270 + 6 * st.wSecond) * 3.14 / 180)),
+				int(200 + 85 * sin((270 + 6 * st.wSecond) * 3.14 / 180)));
 
-		MoveToEx(hdc, 200, 200, NULL);
-		LineTo(hdc, int(200 + 70 * cos((270 + 6 * st.wMinute + st.wSecond / 10) * 3.14 / 180)),
-			int(200 + 70 * sin((270 + 6 * st.wMinute + st.wSecond / 10) * 3.14 / 180)));
+			//분침
+			MyPen = CreatePen(PS_SOLID, 3, RGB(0, 255, 0));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 70 * cos((270 + 6 * st.wMinute + st.wSecond / 10) * 3.14 / 180)),
+				int(200 + 70 * sin((270 + 6 * st.wMinute + st.wSecond / 10) * 3.14 / 180)));
 
-		MyPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
-		OldPen = (HPEN)SelectObject(hdc, MyPen);
-
-		MoveToEx(hdc, 200, 200, NULL);
-		LineTo(hdc, int(200 + 50 * cos((270 + 30 * st.wHour + st.wMinute / 2) * 3.14 / 180)),
-			int(200 + 50 * sin((270 + 30 * st.wHour + st.wMinute / 2) * 3.14 / 180)));
+			//시침
+			MyPen = CreatePen(PS_SOLID, 5, RGB(0, 0, 255));
+			OldPen = (HPEN)SelectObject(hdc, MyPen);
+			MoveToEx(hdc, 200, 200, NULL);
+			LineTo(hdc, int(200 + 50 * cos((270 + 30 * st.wHour + st.wMinute / 2) * 3.14 / 180)),
+				int(200 + 50 * sin((270 + 30 * st.wHour + st.wMinute / 2) * 3.14 / 180)));
+		}
 
 		EndPaint(hWnd, &ps);
 		break;
